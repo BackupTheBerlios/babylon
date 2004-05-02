3817aa55e3c986b204c067b147ffcb266d5eeb86
det <det>
2004-05-02 16:18:50 +0000

    pruefung ob wir uns auf babylon.berlios.de befinden korrigiert

--- babylon/forum/leiste-unten.php
+++ babylon/forum/leiste-unten.php
@@ -14,7 +14,7 @@ function leiste_unten ()
             <tr>
               <td width=\"33%\" height=\"100%\" valign=\"bottom\">";
 
-  if (strchr ($_SERVER["DOCUMENT_ROOT"], 'babylon.berlios.de'))
+  if (substr_count ($_SERVER["DOCUMENT_ROOT"], 'babylon.berlios.de'))
     echo "                Danke f&uuml;rs Hosten!<br>
                 <A href=\"http://developer.berlios.de\">
                   <IMG src=\"http://developer.berlios.de/bslogo.php?group_id=1674\" width=\"124\" height=\"32\" border=\"0\" alt=\"BerliOS Developer Logo\">
