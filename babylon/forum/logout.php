73bd70117335d1c6dbfbec4dee7d94c133ac4743
det <det>
2004-07-04 10:24:25 +0000

    Der Zeitstempel des Logout wird zusaetzlich in der Datenbank abgelegt. Wird fuer den Posteingang benoetigt.

--- babylon/forum/logout.php
+++ babylon/forum/logout.php
@@ -31,8 +31,9 @@ if (isset ($_COOKIE["$B_cookie_id"]) and isset ($_COOKIE["$B_cookie_sw"]))
     {
       setcookie($B_cookie_id, '', NULL, '/');
       setcookie($B_cookie_sw, '', NULL, '/');
+      $stempel = time ();
       mysql_query ("UPDATE Benutzer
-                    SET Eingeloggt=\"n\", Cookie=NULL
+                    SET Eingeloggt=\"n\", Cookie=NULL, LogoutStempel=\"$stempel\"
                     WHERE BenutzerId=\"$id\"")
         or die ('F0054: Es ist nicht m&ouml;glich die Datenbank informationen zu aktuallisieren');
     }  
